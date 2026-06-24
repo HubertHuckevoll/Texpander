32c81f993f50ec3908df852edb5ad9795acfeed7
HubertHuckevoll <HubertHuckevoll@users.noreply.github.com>
2026-06-24 13:19:11 +0200

    v3

--- texpander.js
+++ texpander.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Texpander 2
-// @version      2
+// @version      3
 // @description  PVP helper
 // @match        *://*/*
 // @grant        unsafeWindow
@@ -18,9 +18,29 @@
     "use strict";
 
     var win = typeof unsafeWindow !== "undefined" ? unsafeWindow : window;
-    var hotkey = "k";
+    var hotkey = "r";
 
     var sheetUrl = "https://script.google.com/macros/s/AKfycbx09Y7F9ZqoUVQDfkFPGqsmnBfJht4EuFDa_8qiO6CUvB96ydnixTdo0PXeJaMgyj5i/exec";
+
+    var NODE_ELEMENT = 1;
+    var NODE_TEXT = 3;
+
+    var tokenCharRegex = /[\p{L}\p{N}_]/u;
+
+    var style = document.createElement("style");
+    style.textContent =
+        ".texpander-busy{position:relative}" +
+        ".texpander-busy:after{" +
+          "content:'';" +
+          "position:absolute;" +
+          "right:6px;" +
+          "top:6px;" +
+          "width:9px;" +
+          "height:9px;" +
+          "background:red;" +
+          "border-radius:50%;" +
+          "z-index:999999;" +
+        "}";
 
     function requestText(url) {
         return new Promise(function (resolve, reject) {
@@ -80,37 +100,12 @@
             }
         }
 
-        row.push(field);
-        rows.push(row);
+        if (field !== "" || row.length > 0) {
+            row.push(field);
+            rows.push(row);
+        }
 
         return rows;
-    }
-
-    function loadReplacements() {
-        return requestText(sheetUrl).then(function (tsv) {
-            var rows = parseTSV(tsv);
-            var replacements = {};
-            var i;
-            var key;
-            var value;
-
-            console.log("TinyMCE expander parsed rows:", rows);
-
-            for (i = 0; i < rows.length; i++) {
-                if (rows[i].length < 2) {
-                    continue;
-                }
-
-                key = rows[i][0].replace(/\u00a0/g, " ").trim();
-                value = rows[i][1];
-
-                if (key !== "") {
-                    replacements[key] = value;
-                }
-            }
-
-            return replacements;
-        });
     }
 
     function escapeRegExp(text) {
@@ -121,76 +116,259 @@
         return b.length - a.length;
     }
 
-    function buildRegex(replacements) {
-        var keys = Object.keys(replacements).sort(sortByLengthDesc).map(escapeRegExp);
+    function isTokenChar(c) {
+        return tokenCharRegex.test(c);
+    }
+
+    function isPlainTokenKey(key) {
+        var i;
+
+        if (key === "") {
+            return false;
+        }
+
+        for (i = 0; i < key.length; i++) {
+            if (!isTokenChar(key.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function buildComplexRegex(keys) {
+        var escapedKeys;
 
         if (keys.length === 0) {
             return null;
         }
 
+        escapedKeys = keys.sort(sortByLengthDesc).map(escapeRegExp);
+
         return new RegExp(
-            "(^|[^\\p{L}\\p{N}_])(" + keys.join("|") + ")(?=$|[^\\p{L}\\p{N}_])",
+            "(^|[^\\p{L}\\p{N}_])(" + escapedKeys.join("|") + ")(?=$|[^\\p{L}\\p{N}_])",
             "gu"
         );
     }
 
-    function shouldSkipNode(node) {
-        var element = node.parentNode;
+    function compileReplacements(rows) {
+        var replacements = Object.create(null);
+        var complexKeys = [];
+        var plainCount = 0;
+        var complexCount = 0;
+        var i;
+        var key;
+        var value;
+        var alreadyKnown;
+
+        for (i = 0; i < rows.length; i++) {
+            if (rows[i].length < 2) {
+                continue;
+            }
+
+            key = rows[i][0].replace(/\u00a0/g, " ").trim();
+            value = rows[i][1];
+
+            if (key === "") {
+                continue;
+            }
+
+            alreadyKnown = Object.prototype.hasOwnProperty.call(replacements, key);
+            replacements[key] = value;
+
+            if (alreadyKnown) {
+                continue;
+            }
+
+            if (isPlainTokenKey(key)) {
+                plainCount++;
+            } else {
+                complexKeys.push(key);
+                complexCount++;
+            }
+        }
+
+        return {
+            replacements: replacements,
+            complexRegex: buildComplexRegex(complexKeys),
+            plainCount: plainCount,
+            complexCount: complexCount
+        };
+    }
+
+    function loadReplacements() {
+        return requestText(sheetUrl).then(function (tsv) {
+            var rows = parseTSV(tsv);
+            var compiled = compileReplacements(rows);
+
+            console.log(
+                "TinyMCE expander loaded " +
+                compiled.plainCount +
+                " plain keys and " +
+                compiled.complexCount +
+                " complex keys"
+            );
+
+            return compiled;
+        });
+    }
+
+    function appendExpandedTokenText(state, text, replacements) {
+        var i = 0;
+        var last = 0;
+        var start;
+        var token;
+        var replacement;
+
+        while (i < text.length) {
+            if (!isTokenChar(text.charAt(i))) {
+                i++;
+                continue;
+            }
+
+            start = i;
+
+            while (i < text.length && isTokenChar(text.charAt(i))) {
+                i++;
+            }
+
+            token = text.slice(start, i);
+
+            if (Object.prototype.hasOwnProperty.call(replacements, token)) {
+                replacement = replacements[token];
+
+                if (last < start) {
+                    state.parts.push(text.slice(last, start));
+                }
+
+                state.parts.push(replacement);
+                last = i;
+                state.changed = true;
+            }
+        }
+
+        if (last < text.length) {
+            state.parts.push(text.slice(last));
+        }
+    }
+
+    function expandText(text, compiled) {
+        var replacements = compiled.replacements;
+        var complexRegex = compiled.complexRegex;
+        var state = {
+            parts: [],
+            changed: false
+        };
+        var match;
+        var prefix;
+        var key;
+        var keyStart;
+        var keyEnd;
+        var last = 0;
+
+        if (!complexRegex) {
+            appendExpandedTokenText(state, text, replacements);
+
+            if (!state.changed) {
+                return null;
+            }
+
+            return state.parts.join("");
+        }
+
+        complexRegex.lastIndex = 0;
+
+        while ((match = complexRegex.exec(text)) !== null) {
+            prefix = match[1];
+            key = match[2];
+
+            keyStart = match.index + prefix.length;
+            keyEnd = keyStart + key.length;
+
+            if (last < keyStart) {
+                appendExpandedTokenText(state, text.slice(last, keyStart), replacements);
+            }
+
+            state.parts.push(replacements[key]);
+            state.changed = true;
+            last = keyEnd;
+        }
+
+        if (last < text.length) {
+            appendExpandedTokenText(state, text.slice(last), replacements);
+        }
+
+        if (!state.changed) {
+            return null;
+        }
+
+        return state.parts.join("");
+    }
+
+    function shouldSkipElement(element) {
         var name;
 
-        while (element && element.nodeType === 1) {
-            name = element.nodeName.toLowerCase();
+        if (!element || element.nodeType !== NODE_ELEMENT) {
+            return false;
+        }
 
-            if (name === "script" || name === "style" || name === "code" || name === "pre") {
-                return true;
-            }
+        name = element.nodeName.toLowerCase();
 
-            if (element.getAttribute && element.getAttribute("contenteditable") === "false") {
-                return true;
-            }
+        if (name === "script" || name === "style" || name === "code" || name === "pre") {
+            return true;
+        }
 
-            element = element.parentNode;
+        if (element.getAttribute && element.getAttribute("contenteditable") === "false") {
+            return true;
         }
 
         return false;
     }
 
-    function expandEditor(editor, replacements) {
+    function expandEditor(editor, compiled) {
         var body = editor.getBody();
-        var doc = editor.getDoc();
-        var regex = buildRegex(replacements);
-        var walker;
+        var stack = [];
         var node;
+        var child;
         var oldText;
         var newText;
         var changed = 0;
 
-        if (!body || !doc || !regex) {
+        if (!body || !compiled) {
             return 0;
         }
 
         function doReplace() {
-            walker = doc.createTreeWalker(
-                body,
-                4,
-                {
-                    acceptNode: function (textNode) {
-                        return shouldSkipNode(textNode) ? 2 : 1;
+            stack.push(body);
+
+            while (stack.length > 0) {
+                node = stack.pop();
+
+                if (node.nodeType === NODE_TEXT) {
+                    oldText = node.nodeValue;
+                    newText = expandText(oldText, compiled);
+
+                    if (newText !== null) {
+                        node.nodeValue = newText;
+                        changed++;
                     }
+
+                    continue;
                 }
-            );
 
-            while ((node = walker.nextNode())) {
-                oldText = node.nodeValue;
-                regex.lastIndex = 0;
+                if (node.nodeType !== NODE_ELEMENT) {
+                    continue;
+                }
 
-                newText = oldText.replace(regex, function (match, prefix, key) {
-                    return prefix + replacements[key];
-                });
+                if (shouldSkipElement(node)) {
+                    continue;
+                }
 
-                if (newText !== oldText) {
-                    node.nodeValue = newText;
-                    changed++;
+                child = node.lastChild;
+
+                while (child) {
+                    stack.push(child);
+                    child = child.previousSibling;
                 }
             }
         }
@@ -218,14 +396,36 @@
         return changed;
     }
 
+    function setBusy(editor, on) {
+        var container;
+
+        if (!editor || !editor.getContainer) {
+            return;
+        }
+
+        container = editor.getContainer();
+
+        if (container && container.classList) {
+            container.classList.toggle("texpander-busy", on);
+        }
+    }
+
     function expandWithSheet(editor) {
         if (!editor) {
             console.log("TinyMCE expander: no editor");
             return;
         }
 
-        loadReplacements().then(function (replacements) {
-            var changed = expandEditor(editor, replacements);
+        if (editor._texpanderBusy) {
+            console.log("TinyMCE expander: already running");
+            return;
+        }
+
+        editor._texpanderBusy = true;
+        setBusy(editor, true);
+
+        loadReplacements().then(function (compiled) {
+            var changed = expandEditor(editor, compiled);
 
             if (changed === 0) {
                 console.log("TinyMCE expander: nothing changed");
@@ -234,6 +434,9 @@
             }
         }).catch(function (err) {
             console.log("TinyMCE expander: " + err.message);
+        }).then(function () {
+            editor._texpanderBusy = false;
+            setBusy(editor, false);
         });
     }
 
@@ -266,6 +469,12 @@
         expandWithSheet(editor);
     }
 
+    function makeEditorKeydownHandler(editor) {
+        return function (event) {
+            handleHotkey(event, editor);
+        };
+    }
+
     function installEditorHotkeys() {
         var tiny = win.tinymce || window.tinymce;
         var editors;
@@ -283,12 +492,7 @@
             }
 
             editors[i]._texpanderHotkeyInstalled = true;
-
-            (function (editor) {
-                editor.on("keydown", function (event) {
-                    handleHotkey(event, editor);
-                });
-            })(editors[i]);
+            editors[i].on("keydown", makeEditorKeydownHandler(editors[i]));
         }
     }
 
@@ -297,4 +501,10 @@
     }, true);
 
     window.setInterval(installEditorHotkeys, 1000);
+
+    if (document.head) {
+        document.head.appendChild(style);
+    } else {
+        document.documentElement.appendChild(style);
+    }
 })();
